a60cb1dce00eff63f8f36ee908dd7642bf68ad2a
Mamoona Ramzan <103951283+MamoonaRamzan@users.noreply.github.com>
2025-07-12 21:48:21 +0500

    updated params and prediction pipeline

--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -1,27 +1,28 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
-from tensorflow.keras.models import load_model 
-from tensorflow.keras.preprocessing import image 
-import os 
+import os
 
 class PredictionPipeline:
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.filename = filename
 
     def predict(self):
-    # load model
-        model = load_model(os.path.join("artifacts","training","model.h5"))
+        # Load the trained model
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
-        imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
+        # Load and preprocess the image
+        test_image = image.load_img(self.filename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+        test_image = np.expand_dims(test_image, axis=0)
+        test_image = test_image / 255.0  # same rescale as training
 
-        if result[0] == 1:
-            prediction = 'yes'
-            return [{ "image" : prediction}]
-        else:
-            prediction = 'no'
-            return [{ "image" : prediction}]
-    
+        # Predict
+        predictions = model.predict(test_image)
+        predicted_index = np.argmax(predictions, axis=1)[0]
+
+        # Class index mapping (alphabetical order)
+        class_labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
+        predicted_class = class_labels[predicted_index]
+
+        return [{"Result": predicted_class}]
